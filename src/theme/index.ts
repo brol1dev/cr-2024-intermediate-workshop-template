f36737220324583f5d5f8c171f1e311ab1798b3e
brol1dev <eric.vargas.mx@gmail.com>
2024-07-17 09:46:43 -0700

    Add Dark color theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,4 +1,41 @@
-export * from "./colors"
-export * from "./spacing"
-export * from "./typography"
-export * from "./timing"
+import { colors as colorsLight } from "./colors"
+import { colors as colorsDark } from "./colorsDark"
+import { spacing as spacingLight } from "./spacing"
+import { spacing as spacingDark } from "./spacingDark"
+import { timing } from "./timing"
+import { typography } from "./typography"
+
+// This supports "light" and "dark" themes by default. If undefined, it'll use the system theme
+export type ThemeContexts = "light" | "dark" | undefined
+
+// Because we have two themes, we need to define the types for each of them.
+// colorsLight and colorsDark should have the same keys, but different values.
+export type Colors = typeof colorsLight | typeof colorsDark
+// The spacing type needs to take into account the different spacing values for light and dark themes.
+export type Spacing = typeof spacingLight | typeof spacingDark
+
+// These two are consistent across themes.
+export type Timing = typeof timing
+export type Typography = typeof typography
+
+// The overall Theme object should contain all of the data you need to style your app.
+export interface Theme {
+    colors: Colors;
+    spacing: Spacing;
+    typography: Typography;
+    timing: Timing;
+  }
+  
+  // Here we define our themes.
+  export const lightTheme: Theme = {
+    colors: colorsLight,
+    spacing: spacingLight,
+    typography,
+    timing,
+  };
+  export const darkTheme: Theme = {
+    colors: colorsDark,
+    spacing: spacingDark,
+    typography,
+    timing,
+  };
