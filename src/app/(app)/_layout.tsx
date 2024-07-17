77e8c8507273b1098df7e526a97c1ac4674ebf64
brol1dev <eric.vargas.mx@gmail.com>
2024-07-17 14:31:33 -0700

    Add profile quick action

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,4 +1,7 @@
 import React from "react"
+import { Platform } from "react-native"
+import * as QuickActions from "expo-quick-actions"
+import { useQuickActionRouting, RouterAction } from "expo-quick-actions/router"
 import { Redirect, Stack } from "expo-router"
 import { observer } from "mobx-react-lite"
 import { useStores } from "src/models"
@@ -11,6 +14,20 @@ export default observer(function Layout() {
       profile: { darkMode },
     },
   } = useStores()
+
+  useQuickActionRouting()
+
+  React.useEffect(() => {
+    QuickActions.setItems<RouterAction>([
+      {
+        title: "Update your profile",
+        subtitle: "Keep your deets up-to-date!",
+        icon: Platform.OS === "android" ? undefined : "contact", // we'll come back to the Android icon
+        id: "0",
+        params: { href: "/(app)/(tabs)/profile" },
+      },
+    ])
+  }, [])
 
   const { themeScheme, setThemeContextOverride, ThemeProvider } = useThemeProvider(
     darkMode ? "dark" : "light",
